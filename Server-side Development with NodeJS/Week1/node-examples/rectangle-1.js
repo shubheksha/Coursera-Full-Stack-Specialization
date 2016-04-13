3aa45ad69115c2ffedce701339c761ef80af620f
Shubheksha Jalan <shubheksha@outlook.com>
2016-04-13 19:23:19 +0530

    Week1 exercises added

---
+++ Server-side Development with NodeJS/Week1/node-examples/rectangle-1.js
@@ -0,0 +1,6 @@
+exports.perimeter = function (x, y) {
+           return (2*(x+y));
+ }
+ exports.area = function (x, y) {
+            return (x*y);
+ }
---
+++ Server-side Development with NodeJS/Week1/node-express/server-4.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+app.use(morgan('dev'));
+var dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+.all(function(req, res, next){
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	next();
+})
+.get(function(req, res, next){
+	res.end('Getting all the dishes for you!');
+})
+.delete(function(req, res, next){
+	res.end('Deleting all the dishes!');
+})
+.post(function(req, res, next){
+	res.end('Creating a dish with ' +req.body.name+'with details: '+req.body.description);
+});
+
+
+dishRouter.route('/:dishId')
+.all(function(req, res, next){
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	next();
+})
+.get(function(req, res, next){
+	res.end('Sending details of dish with ' +req.params.dishId+'\n');
+})
+.put(function(req, res, next){
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+})
+.delete(function(req, res, next){
+        res.end('Deleting dish: ' + req.params.dishId);
+});
+
+app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname+'/public'));
+
+
+app.listen(port, hostname, function(){
+	  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
