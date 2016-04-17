7fe09df2fd6b36d16bf9c07a9c0a5513d0a2db8c
Shubheksha Jalan <shubheksha@outlook.com>
2016-04-17 19:25:35 +0530

    Authenthication using browser session based cookies

--- Server-side Development with NodeJS/Week3/basic-auth/server.js
+++ Server-side Development with NodeJS/Week3/basic-auth/server.js
@@ -1,31 +1,49 @@
 var express = require('express');
 var morgan = require('morgan');
+var cookieParse = require('cookie-parser');
 var hostname = 'localhost';
 var port = 3000;
 
 var app = express();
 app.use(morgan('dev'));
+app.use(cookieParse('12345-67890-09876-54321'));
 function auth(req, res, next){
 	console.log(req.headers);
-	var authHeader = req.headers.authorization;
-	if(!authHeader){
-		var err = new Error('No authentication details provided!');
-		err.status = 401;
-		next(err);
-		return;
+	if(!req.signedCookies.user){
+		var authHeader = req.headers.authorization;
+		if(!authHeader){
+			var err = new Error('No authentication details provided!');
+			err.status = 401;
+			next(err);
+			return;
+		}
+		var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+		var user = auth[0];
+		var pass = auth[1];
+		if(user == 'admin' && pass == 'password'){
+			res.cookie('user', 'admin', {signed: true});
+			next();
+		}
+		else{
+			var err = new Error('Authentication failed.\n');
+			err.status = 401;
+			next(err);
+		}
 	}
-var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-var user = auth[0];
-var pass = auth[1];
-if(user == 'admin' && pass == 'password'){
-	next();
+	else{
+		if(req.signedCookies.user == 'admin'){
+			console.log(req.signedCookies);
+			next();
+		}
+		else{
+			var err = new Error('Authentication failed.\n');
+			err.status = 401;
+			next(err);
+		}
+	}
 }
-else{
-	var err = new Error('Authentication failed.\n');
-	err.status = 401;
-	next(err);
-}
-}
+
+
 
 app.use(auth);
 app.use(express.static(__dirname+'/public'));
